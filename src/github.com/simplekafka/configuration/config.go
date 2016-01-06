b760d6a7cd364d9f8e471cb5ddf817164a19cb1c
Ankur Maharjan <akrmhrjn@MacMini.local>
2016-01-06 16:44:26 +0545

    added dockerfile

--- src/github.com/simplekafka/configuration/config.go
+++ src/github.com/simplekafka/configuration/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	//file = "/Users/akrmhrjn/IdeaProjects/simple-kafka/src/github.com/simplekafka/configuration/config.yml"
 	file = "/go/src/github.com/bajratech/backendcontroller/configuration/config.yml"
 )
 
